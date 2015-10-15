4f8ad490d9e9fdf2753af2489705f6444174ff2e
Gronner <felix.braeunling@t-online.de>
2015-10-15 16:57:11 +0200

    Added, adapted tests for Formula.thrust()

--- Test/FormulaTest.py
+++ Test/FormulaTest.py
@@ -1,17 +1,23 @@
 import unittest
+import sys
+sys.path.append('/home/felix/Projekte/Studienarbeit/RocketSim')
+
 import Formula
 
 
 class ThrustTest(unittest.TestCase):
 
     def test_ThrustZero(self):
-        self.assertTrue(Formula.Thrust(0.0, 0.0, 0.0, 0.0, 0.0) == 0, "[!] Zero test failed!")
+        self.assertTrue(Formula.thrust(0.0, 0.0, 0.0, 0.0, 0.0) == 0.0, "[!] Zero test failed!")
 
-    def test_ThrustNoneZero(self):
-        self.assertTrue(Formula.Thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 15, "[!] None-zero test failed")
+    def test_ThrustPositive(self):
+        self.assertTrue(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 225.0, "[!] Positive test failed")
+
+    def test_ThrustNegative(self):
+        self.assertTrue(Formula.thrust(-10.0, 12.0, -15.0, 10.0, 3.0) == -225.0, "[!] Positive test failed")
 
     def test_ThrustFalseNoneZero(self):
-        self.assertFalse(Formula.Thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 10, "[!] False None-zero test failed")
+        self.assertFalse(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 10.0, "[!] False None-zero test failed")
 
 
 def main():
