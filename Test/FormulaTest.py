e0aacbdc837dda57c829b35541b942ede2586f55
Gronner <felix.braeunling@t-online.de>
2015-10-15 17:31:00 +0200

    Added unit test for Formula.drag()

--- Test/FormulaTest.py
+++ Test/FormulaTest.py
@@ -17,7 +17,7 @@ class ThrustTest(unittest.TestCase):
         self.assertTrue(Formula.thrust(-10.0, 12.0, -15.0, 10.0, 3.0) == -225.0, "[!] Negative test failed")
 
     def test_ThrustFalseNoneZero(self):
-        self.assertFalse(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 10.0, "[!] False None-zero test failed")
+        self.assertFalse(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 10.0, "[!] False none-zero test failed")
 
 
 class DragTest(unittest.TestCase):
@@ -35,7 +35,21 @@ class DragTest(unittest.TestCase):
         self.assertTrue(Formula.drag(10.0, -12.0, 15.0, 10.0) == 108000.0, "[!] Negative square test failed!")
 
     def test_DragFalseNoneZero(self):
-        self.assertFalse(Formula.drag(10.0, 12.0, 15.0, 10.0) == 15, "[!] Negative square test failed!")
+        self.assertFalse(Formula.drag(10.0, 12.0, 15.0, 10.0) == 15, "[!] False none-zero test failed!")
+
+class GravityTest(unittest.TestCase):
+
+    def test_GravityZero(self):
+        self.assertRaises(ZeroDivisionError, Formula.gravity(0.0, 0.0, 0.0), "[!] Zero devision test failed!")
+
+    def test_GravityPositve(self):
+        self.assertTrue(Formula.gravity(10.0, 12.0, 15.0) == 5.339264e-10, "[!] Positive test failed!")
+
+    def test_GravityNegative(self):
+        self.assertRaises(ValueError, Formula.gravity(-10.0, 12.0, 15.0), "[!] Negative test failed!")
+
+    def test_GravityFalseNoneZero(self):
+        self.assertFalse(Formula.gravity(10.0, 12.0, 15.0) == 10, "[!] False none-zero test failed!")
 
 def main():
     unittest.main()
