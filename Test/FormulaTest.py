a6cca9a342c7870005e52c4a340a8987b691518f
Gronner <felix.braeunling@t-online.de>
2015-10-17 20:58:30 +0200

    Changed unit testing from assertTrue to assertEqual or assertNotEqual for better feedback

--- Test/FormulaTest.py
+++ Test/FormulaTest.py
@@ -8,34 +8,34 @@ import Formula
 class ThrustTest(unittest.TestCase):
 
     def test_ThrustZero(self):
-        self.assertTrue(Formula.thrust(0.0, 0.0, 0.0, 0.0, 0.0) == 0.0, "[!] Thrust zero test failed!")
+        self.assertEqual(Formula.thrust(0.0, 0.0, 0.0, 0.0, 0.0), 0.0)
 
     def test_ThrustPositive(self):
-        self.assertTrue(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 225.0, "[!] Thrust positive test failed")
+        self.assertEqual(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0), 225.0)
 
     def test_ThrustNegative(self):
-        self.assertTrue(Formula.thrust(-10.0, 12.0, -15.0, 10.0, 3.0) == -225.0, "[!] Thrust negative test failed")
+        self.assertEqual(Formula.thrust(-10.0, 12.0, -15.0, 10.0, 3.0), -225.0)
 
     def test_ThrustFalseNonZero(self):
-        self.assertFalse(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0) == 10.0, "[!] Thrust false non-zero test failed")
+        self.assertNotEqual(Formula.thrust(10.0, 12.0, 15.0, 10.0, 3.0), 10.0)
 
 
 class DragTest(unittest.TestCase):
 
     def test_DragZero(self):
-        self.assertTrue(Formula.drag(0.0, 0.0, 0.0, 0.0) == 0, "[!] Drag zero test failed!")
+        self.assertEqual(Formula.drag(0.0, 0.0, 0.0, 0.0), 0.0)
 
     def test_DragPositive(self):
-        self.assertTrue(Formula.drag(10.0, 12.0, 15.0, 10.0) == 108000.0, "[!] Drag positive test failed!")
+        self.assertEqual(Formula.drag(10.0, 12.0, 15.0, 10.0), 108000.0)
 
     def test_DragNegative(self):
-        self.assertTrue(Formula.drag(-10.0, 12.0, 15.0, 10.0) == -108000.0, "[!] Drag negative test failed!")
+        self.assertEqual(Formula.drag(-10.0, 12.0, 15.0, 10.0), -108000.0)
 
     def test_DragNegativeSquare(self):
-        self.assertTrue(Formula.drag(10.0, -12.0, 15.0, 10.0) == 108000.0, "[!] Drag negative square test failed!")
+        self.assertEqual(Formula.drag(10.0, -12.0, 15.0, 10.0), 108000.0)
 
     def test_DragFalseNonZero(self):
-        self.assertFalse(Formula.drag(10.0, 12.0, 15.0, 10.0) == 15, "[!] Drag false non-zero test failed!")
+        self.assertNotEqual(Formula.drag(10.0, 12.0, 15.0, 10.0), 15.0)
 
 
 class GravityTest(unittest.TestCase):
@@ -45,14 +45,14 @@ class GravityTest(unittest.TestCase):
             Formula.gravity(0.0, 0.0, 0.0)
 
     def test_GravityPositve(self):
-        self.assertTrue(Formula.gravity(10.0, 12.0, 15.0) == 3.559509333333333e-10, "[!] Gravity positive test failed!")
+        self.assertEqual(Formula.gravity(10.0, 12.0, 15.0), 3.559509333333333e-10)
 
     def test_GravityNegative(self):
         with self.assertRaises(ValueError):
             Formula.gravity(-10.0, 12.0, 15.0)
 
     def test_GravityFalseNonZero(self):
-        self.assertFalse(Formula.gravity(10.0, 12.0, 15.0) == 10, "[!] Gravity false non-zero test failed!")
+        self.assertNotEqual(Formula.gravity(10.0, 12.0, 15.0), 10.0)
 
 
 class PressureTest(unittest.TestCase):
@@ -62,32 +62,28 @@ class PressureTest(unittest.TestCase):
             Formula.pressure(0.0, 0.0, 0.0, 0.0, 0.0)
 
     def testPressurePositive(self):
-        self.assertTrue(Formula.pressure(1013.25, 0.0065, 100.0, 0.0, 24.0) == 877.0988519626972, "\
-        [!] Pressure positive test failed!")
+        self.assertEqual(Formula.pressure(1013.25, 0.0065, 100.0, 0.0, 24.0), 877.0988519626972)
 
     def testPressureNegative(self):
-        self.assertTrue(Formula.pressure(-1013.25, 0.0065, 100.0, 0.0, 24.0) == -877.0988519626972, "\
-        [!] Pressure negative test failed!")
+        self.assertEqual(Formula.pressure(-1013.25, 0.0065, 100.0, 0.0, 24.0), -877.0988519626972)
 
     def testPressureFalseNonZero(self):
-        self.assertFalse(Formula.pressure(-1013.25, 0.0065, 100.0, 0.0, 24.0) == -87.098852, "\
-        [!] Pressure false non-zero test failed!")
+        self.assertNotEqual(Formula.pressure(-1013.25, 0.0065, 100.0, 0.0, 24.0), -87.098852)
 
 
 class TemperatureTest(unittest.TestCase):
 
     def test_TemperatureZero(self):
-        self.assertTrue(Formula.temperature(0.0, 0.0, 0.0, 0.0) == 0.0, "[!] Temperature zero test failed!")
+        self.assertEqual(Formula.temperature(0.0, 0.0, 0.0, 0.0), 0.0)
 
     def test_TemperaturePositive(self):
-        self.assertTrue(Formula.temperature(12.0, 3.0, 0.0, 100.0) == -3588, "[!] Temperature positive Test failed!")
+        self.assertEqual(Formula.temperature(12.0, 3.0, 0.0, 100.0), -3588)
 
     def test_TemperatureNegative(self):
-        self.assertTrue(Formula.temperature(12.0, -3.0, 0.0, 100.0) == 3612.0, "[!] Temperature negative Test failed!")
+        self.assertEqual(Formula.temperature(12.0, -3.0, 0.0, 100.0), 3612.0)
 
     def test_TemperatureFalseNonZero(self):
-        self.assertFalse(Formula.temperature(12.0, 3.0, 0.0, -100) == 300.0, "\
-        [!] Temperature false non-zero Test failed!")
+        self.assertNotEqual(Formula.temperature(12.0, 3.0, 0.0, -100), 300.0)
 
 
 class DensityTest(unittest.TestCase):
@@ -97,29 +93,28 @@ class DensityTest(unittest.TestCase):
             Formula.density(0.0, 0.0)
 
     def test_DensityPositive(self):
-        self.assertTrue(Formula.density(10.0, 10.0) == 0.0034832812124127974, "[!] Density positive test failed!")
+        self.assertEqual(Formula.density(10.0, 10.0), 0.0034832812124127974)
 
     def test_DensityNegative(self):
-        self.assertTrue(Formula.density(10.0, -10.0) == -0.0034832812124127974, "[!] Density negative test failed!")
+        self.assertEqual(Formula.density(10.0, -10.0), -0.0034832812124127974)
 
     def test_DensityFalseNonZero(self):
-        self.assertFalse(Formula.density(10.0, 10.0) == 33.0, "[!] Density false non-zero test failed")
+        self.assertNotEqual(Formula.density(10.0, 10.0), 33.0)
 
 
 class ResultingForceTest(unittest.TestCase):
 
     def test_ResultingForceZero(self):
-        self.assertTrue(Formula.resulting_force(0.0, 0.0, 0.0) == 0.0, "[!] Res force zero test failed")
+        self.assertEqual(Formula.resulting_force(0.0, 0.0, 0.0), 0.0)
 
     def test_ResultingForcePositive(self):
-        self.assertTrue(Formula.resulting_force(100.0, 10.0, 1000.0) == 1110.0, "[!] Res force positive test failed!")
+        self.assertEqual(Formula.resulting_force(100.0, 10.0, 1000.0), 1110.0)
 
     def test_ResultingForceNegative(self):
-        self.assertTrue(Formula.resulting_force(-100.0, -10.0, -1000.0) == -1110.0, "[!] Res force negative test failed")
+        self.assertEqual(Formula.resulting_force(-100.0, -10.0, -1000.0), -1110.0)
 
     def test_ResultingForceFalseNonZero(self):
-        self.assertFalse(Formula.resulting_force(-100.0, 10.0, -1000.0) == -1110.0, "\
-        [!] Res force false non-zero test failed")
+        self.assertNotEqual(Formula.resulting_force(-100.0, 10.0, -1000.0), -1110.0)
 
 
 class AngleTest(unittest.TestCase):
@@ -129,14 +124,13 @@ class AngleTest(unittest.TestCase):
             Formula.angle(0.0, 0.0, 0.0, 0.0)
 
     def test_AnglePositive(self):
-        self.assertTrue(Formula.angle(10.0, 3.0, 1000.0, 400.0) == 65.16541251029841, "[!] Angle positive test failed!")
+        self.assertEqual(Formula.angle(10.0, 3.0, 1000.0, 400.0), 65.16541251029841)
 
     def test_AngleNegative(self):
-        self.assertTrue(Formula.angle(-10.0, 3.0, 1000.0, 400.0) == 114.83458748970159, "\
-        [!] Angle negative test failed!")
+        self.assertEqual(Formula.angle(-10.0, 3.0, 1000.0, 400.0), 114.83458748970159)
 
     def testAngleFalseNonZero(self):
-        self.assertFalse(Formula.angle(10.0, 3.0, 1000.0, 400.0) == 14.0, "[!] Angle false non-zero failed!")
+        self.assertNotEqual(Formula.angle(10.0, 3.0, 1000.0, 400.0), 14.0)
 
 
 class AccelerationTest(unittest.TestCase):
@@ -146,88 +140,88 @@ class AccelerationTest(unittest.TestCase):
             Formula.acceleration(0.0, 0.0)
 
     def test_AccelerationPositive(self):
-        self.assertTrue(Formula.acceleration(100.0, 10.0) == 10.0, "[!] Acceleration positive test failed!")
+        self.assertEqual(Formula.acceleration(100.0, 10.0), 10.0)
 
     def test_AccelerationNegative(self):
-        self.assertTrue(Formula.acceleration(100.0, -10.0) == -10.0, "[!] Acceleration negative test failed!")
+        self.assertEqual(Formula.acceleration(100.0, -10.0), -10.0)
 
     def test_AccelerationFalseNonZero(self):
-        self.assertFalse(Formula.acceleration(100.0, 10.0) == 23.0, "[!] Acceleration false non-zero test failed!")
+        self.assertNotEqual(Formula.acceleration(100.0, 10.0), 23.0)
 
 
 class VelocityTest(unittest.TestCase):
 
     def test_VelocityZero(self):
-        self.assertTrue(Formula.velocity(0.0, 0.0, 0.0) == 0.0, "[!] Velocity zero test failed!")
+        self.assertEqual(Formula.velocity(0.0, 0.0, 0.0), 0.0)
 
     def test_VelocityPositive(self):
-        self.assertTrue(Formula.velocity(10.0, 1.0, 9.81) == 19.810000000000002, "[!] Velocity positive test failed!")
+        self.assertEqual(Formula.velocity(10.0, 1.0, 9.81), 19.810000000000002)
 
     def test_VelocityNegative(self):
-        self.assertTrue(Formula.velocity(-10.0, 1.0, -9.81) == -19.810000000000002, "[!] Velocity negative test failed!")
+        self.assertEqual(Formula.velocity(-10.0, 1.0, -9.81), -19.810000000000002)
 
     def test_VelocityFalseNonZero(self):
-        self.assertFalse(Formula.velocity(10.0, 1.0, 9.81) == 14.03, "[!] Velocity false non-zero test failed!")
+        self.assertNotEqual(Formula.velocity(10.0, 1.0, 9.81), 14.03)
 
 
 class ResXTest(unittest.TestCase):
 
     def test_ResXZero(self):
-        self.assertTrue(Formula.res_x(0.0, 0.0) == 0.0, "[!] Res X zero test failed!")
+        self.assertEqual(Formula.res_x(0.0, 0.0), 0.0)
 
     def test_ResXPositive(self):
-        self.assertTrue(Formula.res_x(10.0, 20.0) == 9.396926207859085, "[!] Res X positive test failed!")
+        self.assertEqual(Formula.res_x(10.0, 20.0), 9.396926207859085)
 
     def test_ResXNegative(self):
-        self.assertTrue(Formula.res_x(10.0, -20.0) == 9.396926207859085, "[!] Res X negative test failed!")
+        self.assertEqual(Formula.res_x(10.0, -20.0), 9.396926207859085)
 
     def test_ResXFalseNonZero(self):
-        self.assertFalse(Formula.res_x(10.0, 20.0) == 14.1334, "[!] Res X false non-zero test failed!")
+        self.assertNotEqual(Formula.res_x(10.0, 20.0), 14.1334)
 
 
 class ResYTest(unittest.TestCase):
 
     def test_ResYZero(self):
-        self.assertTrue(Formula.res_y(0.0, 0.0) == 0.0, "[!] Res Y zero test failed!")
+        self.assertEqual(Formula.res_y(0.0, 0.0), 0.0)
 
     def test_ResYPositive(self):
-        self.assertTrue(Formula.res_y(10.0, 20.0) == 3.420201433256687, "[!] Res Y positive test failed!")
+        self.assertEqual(Formula.res_y(10.0, 20.0), 3.420201433256687)
 
     def test_ResYNegative(self):
-        self.assertTrue(Formula.res_y(10.0, -20.0) == -3.420201433256687, "[!] Res Y negative test failed!")
+        self.assertEqual(Formula.res_y(10.0, -20.0), -3.420201433256687)
 
     def test_ResYFalseNonZero(self):
-        self.assertFalse(Formula.res_y(10.0, 20.0) == 14.1334, "[!] Res Y false non-zero test failed!")
+        self.assertNotEqual(Formula.res_y(10.0, 20.0), 14.1334)
 
 
 class WayTest(unittest.TestCase):
 
     def test_WayZero(self):
-        self.assertTrue(Formula.way(0.0, 0.0, 0.0, 0.0) == 0.0, "[!] Way zero test failed!")
+        self.assertEqual(Formula.way(0.0, 0.0, 0.0, 0.0), 0.0)
 
     def test_WayPositive(self):
-        self.assertTrue(Formula.way(10.0, 1.0, 10.0, 9.81) == 24.905, "[!] Way positive test failed!")
+        self.assertEqual(Formula.way(10.0, 1.0, 10.0, 9.81), 24.905)
 
     def test_WayNegative(self):
-        self.assertTrue(Formula.way(-10.0, -1.0, 10.0, 9.81) == -15.094999999999999, "[!] Way negative test failed!")
+        self.assertEqual(Formula.way(-10.0, -1.0, 10.0, 9.81), -15.094999999999999)
 
     def test_WayFalseNonZero(self):
-        self.assertFalse(Formula.way(10.0, 1.0, 10.0, 9.81) == 12.032, "[!] Way false non-zero test failed!")
+        self.assertNotEqual(Formula.way(10.0, 1.0, 10.0, 9.81), 12.032)
 
 
 class PositionTest(unittest.TestCase):
 
     def test_PositionZero(self):
-        self.assertTrue(Formula.position(0.0, 0.0) == 0.0, "[!] Position zero test failed!")
+        self.assertEqual(Formula.position(0.0, 0.0), 0.0)
 
     def test_PositionPositive(self):
-        self.assertTrue(Formula.position(20.0, 123.0) == 143.00, "[!] Position positive test failed!")
+        self.assertEqual(Formula.position(20.0, 123.0), 143.00)
 
     def test_PositionNegative(self):
-        self.assertTrue(Formula.position(-20.0, -123.0) == -143.00, "[!] Position negative test failed!")
+        self.assertEqual(Formula.position(-20.0, -123.0), -143.00)
 
     def test_PositionFalseNonZero(self):
-        self.assertFalse(Formula.position(30.0, -44.0) == -143.00, "[!] Position false non-zero test failed!")
+        self.assertNotEqual(Formula.position(30.0, -44.0), -143.00)
 
 
 def main():
