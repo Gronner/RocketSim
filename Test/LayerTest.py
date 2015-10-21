233eab5e29efa94dd1931c14bf64ca4f8f2bc433
Gronner <felix.braeunling@t-online.de>
2015-10-21 10:53:14 +0200

    Added unit tests for Layers.layer.get_temp_low() and set_temp_low()

--- Test/LayerTest.py
+++ Test/LayerTest.py
@@ -41,6 +41,23 @@ class WidthTest(unittest.TestCase):
         self.layer_set.set_temp_gradient(self.temp_gradient)
         self.assertEqual(self.layer_set.get_temp_gradient(), self.temp_gradient)
 
+    def test_GetTempLow(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer = Layer(self.width_layer, self.temp_gradient, self.temp_low, self.pressure_low)
+        self.assertEqual(self.layer.get_temp_low(), self.temp_low)
+
+    def test_SetTempLow(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer_set = Layer(0.0, 0.0, 0.0, 0.0)
+        self.layer_set.set_temp_low(self.temp_low)
+        self.assertEqual(self.layer_set.get_temp_low(), self.temp_low)
+
 
 def main():
     unittest.main()
