704baf3ffc48bd502b0f16368876e78d5c125f50
Gronner <felix.braeunling@t-online.de>
2015-10-21 10:20:52 +0200

    Fixed unit tests for Layers.Layer.getWidth() and setWidth()

--- Test/LayerTest.py
+++ Test/LayerTest.py
@@ -7,17 +7,26 @@ from Layers import Layer
 
 class WidthTest(unittest.TestCase):
 
-    def setUp(self):
-        self.width_layer = 18000.0 # [m]
-        self.temp_gradient = 6.5 / 1000.0 # [K/m]
-        self.temp_low = 30.0 + 272.5 # [K]
-        self.pressure_low = 101325.0 # [Pa]
+    def test_GetWidthTestTrue(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
         self.layer = Layer(self.width_layer, self.temp_gradient, self.temp_low, self.pressure_low)
-        self.layer_set = Layer(0.0, 0.0, 0.0, 0.0)
-
-    def GetWidthTestTrue(self):
         self.assertEqual(self.layer.get_width(), self.width_layer)
 
-    def SetWidthTest(self):
+    def test_SetWidthTest(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer_set = Layer(0.0, 0.0, 0.0, 0.0)
         self.layer_set.set_width(self.width_layer)
         self.assertEqual(self.layer_set.get_width(), self.width_layer)
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
