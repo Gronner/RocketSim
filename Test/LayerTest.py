791ca74b245bc7806497bd6c5bbb9cc75129d64a
Gronner <felix.braeunling@t-online.de>
2015-10-21 10:32:49 +0200

    Added unit tests for Layers.layer.set_temp_gradient() and get_temp_gradient()

--- Test/LayerTest.py
+++ Test/LayerTest.py
@@ -24,6 +24,23 @@ class WidthTest(unittest.TestCase):
         self.layer_set.set_width(self.width_layer)
         self.assertEqual(self.layer_set.get_width(), self.width_layer)
 
+    def test_GetTempGradient(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer = Layer(self.width_layer, self.temp_gradient, self.temp_low, self.pressure_low)
+        self.assertEqual(self.layer.get_temp_gradient(), self.temp_gradient)
+
+    def test_SetTempGradient(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer_set = Layer(0.0, 0.0, 0.0, 0.0)
+        self.layer_set.set_temp_gradient(self.temp_gradient)
+        self.assertEqual(self.layer_set.get_temp_gradient(), self.temp_gradient)
+
 
 def main():
     unittest.main()
