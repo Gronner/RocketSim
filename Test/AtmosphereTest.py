28f97704f9e07c97f08e762eac4467bb13d62dd3
Gronner <felix.braeunling@t-online.de>
2015-10-25 18:36:20 +0100

    Added unit tests for Atmosphere.atmosphere.get_layer()

--- Test/AtmosphereTest.py
+++ Test/AtmosphereTest.py
@@ -35,3 +35,36 @@ class AddLayerTest(unittest.TestCase):
             self.atmosphere.add_layer(self.layer_two)
         except Exception, e:
             self.fail(e)
+
+
+class GetLayerTest(unittest.TestCase):
+
+    def test_GetLayerNoLayer(self):
+        self.atmosphere = Atmosphere()
+        self.height_rocket = 10000.0
+        with self.assertRaises(IndexError):
+            self.atmosphere.get_layer(self.height_rocket)
+
+    def test_GetLayerOneLayer(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer_one = Layer(self.width_layer, self.temp_gradient, self.temp_low, self.pressure_low)
+        self.atmosphere = Atmosphere()
+        self.height_rocket = 10000.0
+        self.atmosphere.add_layer(self.layer_one)
+        self.assertEqual(self.atmosphere.get_layer(self.height_rocket), self.layer_one)
+
+    def test_GetLayerNLayer(self):
+        self.width_layer = 18000.0  # [m]
+        self.temp_gradient = 6.5 / 1000.0  # [K/m]
+        self.temp_low = 30.0 + 272.5  # [K]
+        self.pressure_low = 101325.0  # [Pa]
+        self.layer_one = Layer(self.width_layer, self.temp_gradient, self.temp_low, self.pressure_low)
+        self.layer_two = Layer(self.width_layer, self.temp_gradient, self.temp_low, self.pressure_low)
+        self.atmosphere = Atmosphere()
+        self.height_rocket = 10000.0
+        self.atmosphere.add_layer(self.layer_one)
+        self.atmosphere.add_layer(self.layer_two)
+        self.assertEqual(self.atmosphere.get_layer(self.height_rocket), self.layer_one)
