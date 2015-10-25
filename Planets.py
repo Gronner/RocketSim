4c0ba06203ac8c1a590c3a52987ae2552cb276b6
Gronner <felix.braeunling@t-online.de>
2015-10-25 21:46:40 +0100

    Implemented Planets.Planet.get_pos()

--- Planets.py
+++ Planets.py
@@ -11,7 +11,7 @@ class Planet(object):
         Setup of an object that describes a planet (or celestial body)
         :param mass_planet: Mass of the planet (Double) [kg]
         :param radius_planet: Radius of the planet (center to surface) (Double) [m]
-        :param pos_planet: Position of the planets center of mass as x and y Coordinates (Tuple -> (Integer, Integer))
+        :param pos_planet: Position of the planets center of mass as x and y Coordinates (Tuple -> (Double, Double))
         """
         self.mass_planet = mass_planet
         self.radius_planet = radius_planet
@@ -28,3 +28,9 @@ class Planet(object):
         :return: Radius of the planet (Double) [m]
         """
         return self.radius_planet
+
+    def get_pos(self):
+        """
+        :return: Position of the center of mass of the planet (Tuple -> (Double, Double)
+        """
+        return self.pos_planet
