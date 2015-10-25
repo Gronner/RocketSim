7d793c7620153a0c65da46aa630d9207bba48905
Gronner <felix.braeunling@t-online.de>
2015-10-25 21:11:56 +0100

    Fixed unit tests for Planets.Planet.get_mass()

--- Test/PlanetTest.py
+++ Test/PlanetTest.py
@@ -8,16 +8,16 @@ from Planets import Planet
 class GetMassTest(unittest.TestCase):
 
     def test_GetMassZero(self):
-        self.planet = Planet(6000.0, 0.0, (0, 0))
+        self.planet = Planet(0.0, 6000.0, (0, 0))
         self.assertEqual(self.planet.get_mass(), 0.0)
 
     def test_GetMassNonZero(self):
-        self.planet = Planet(6000.0, 100000000.0, (0, 0))
+        self.planet = Planet(100000000.0, 6000.0, (0, 0))
         self.assertEqual(self.planet.get_mass(), 100000000.0)
 
     def test_GetMassFalseNonZero(self):
         self.planet = Planet(6000.0, 100000000.0, (0, 0))
-        self.assertEqual(self.planet.get_mass(), 131231.0)
+        self.assertNotEqual(self.planet.get_mass(), 131231.0)
 
 
 def main():
