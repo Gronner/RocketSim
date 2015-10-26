fbe926f7f48046ca8462013c08873cf3c8ac16e8
Gronner <felix.braeunling@t-online.de>
2015-10-26 17:11:41 +0100

    Updated unit tests to include drag coefficient for RocketParts.RocketPart.__init__

--- Test/RocketPartTest.py
+++ Test/RocketPartTest.py
@@ -8,27 +8,33 @@ from RocketParts import RocketPart
 class InitRocketPartTest(unittest.TestCase):
 
     def test_InitRocketPartZero(self):
-        self.rocket_part = RocketPart(0.0, 0.0)
-        self.assertEqual(self.rocket_part.mass_part, 0.0)
-        self.assertEqual(self.rocket_part.surface_part, 0.0)
+        self.rocket_part = RocketPart(0.0, 0.0, 0.0)
+        self.assertEqual(self.rocket_part.mass_part, 0.0, 0.0)
+        self.assertEqual(self.rocket_part.surface_part, 0.0, 0.0)
 
     def test_InitRocketPartPositive(self):
-        self.rocket_part = RocketPart(100.2, 234.50)
+        self.rocket_part = RocketPart(100.2, 234.50, 145.2)
         self.assertEqual(self.rocket_part.mass_part, 100.2)
         self.assertEqual(self.rocket_part.surface_part, 234.5)
+        self.assertEqual(self.rocket_part.drag_coefficient_part, 234.5)
 
     def test_InitRocketPartMassNegative(self):
         with self.assertRaises(ValueError):
-            self.rocket_part = RocketPart(-110.0, 0.0)
+            self.rocket_part = RocketPart(-110.0, 0.0, 0.0)
 
     def test_InitRocketPartSurfaceNegative(self):
         with self.assertRaises(ValueError):
-            self.rocket_part = RocketPart(0.0, -1100.0)
+            self.rocket_part = RocketPart(0.0, -1100.0, 0.0)
+
+    def test_InitRocketPartDragCoefficientNegative(self):
+        with self.assertRaises(ValueError):
+            self.rocket_part = RocketPart(0.0, 0.0, -1000.0)
 
     def test_InitRockPartFalseNonZero(self):
         self.rocket_part = RocketPart(100.2, 234.50)
         self.assertNotEqual(self.rocket_part.mass_part, 235.2)
         self.assertNotEqual(self.rocket_part.surface_part, 1234.5)
+        self.assertEqual(self.rocket_part.drag_coefficient_part, 234.5)
 
 
 class GetMassTest(unittest.TestCase):
