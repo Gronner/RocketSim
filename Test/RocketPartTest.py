003f0bca8988eea6a29971e8ba0aa5f61e27c74d
Gronner <felix.braeunling@t-online.de>
2015-10-26 16:50:32 +0100

    Changed unit test function name, added main() for unit tests

--- Test/RocketPartTest.py
+++ Test/RocketPartTest.py
@@ -21,7 +21,7 @@ class InitRocketPartTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.rocket_part = RocketPart(-110.0, 0.0)
 
-    def test_InitRocketPartMassNegative(self):
+    def test_InitRocketPartSurfaceNegative(self):
         with self.assertRaises(ValueError):
             self.rocket_part = RocketPart(0.0, -1100.0)
 
@@ -29,3 +29,9 @@ class InitRocketPartTest(unittest.TestCase):
         self.rocket_part = RocketPart(100.2, 234.50)
         self.assertEqual(self.rocket_part.mass_part, 235.2)
         self.assertEqual(self.rocket_part.surface_part, 1234.5)
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
