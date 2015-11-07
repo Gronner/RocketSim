d68cca5fc0f1766f62c449a58f5b78b1a8025d7f
Gronner <felix.braeunling@t-online.de>
2015-11-07 21:46:13 +0100

    Implemented RocketSim.Flight.__init__()

--- RocketSim.py
+++ RocketSim.py
@@ -4,12 +4,21 @@ Author: Felix Braeunling
 Description: This file contains the Flight class, that describes a setup flight and also contains
 """
 
+
 class Flight(object):
 
-    def __init__(self, time_delta, planet, rocket, atmosphere, data, filename):
+    def __init__(self, time_delta, planet, rocket, atmosphere, data):
+        """
+        Setup of a flight with it's parameters
+        :param time_delta: Size of the time step the simulation use
+        :param planet: Planet the flight will take place
+        :param rocket: Rocket the flight will use
+        :param atmosphere: Atmosphere of the planet
+        :param data: Data object gained values will be saved in.
+        :return:
+        """
         self.time_delta = time_delta
         self.planet = planet
         self.rocket = rocket
         self.atmosphere = atmosphere
         self.data = data
-
