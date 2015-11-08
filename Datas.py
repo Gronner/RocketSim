02876b9513a4a6046b4afae780f24f88d1760202
Gronner <felix.braeunling@t-online.de>
2015-11-08 16:24:52 +0100

    Updated Data to use x and y coordinates instead of a vector

--- Datas.py
+++ Datas.py
@@ -11,28 +11,30 @@ class Data(object):
 
     def __init__(self, data_file):
         self.data_file = data_file
-        self.time = []  # [s]
-        self.pos_rocket = []  # [(m, m)] for x and y coordinates
-        self.velocity_rocket = []  # [m/s]
-        self.acceleration_rocket = []  # [m/s^2]
-        self.mass_rocket = []  # [kg]
-        self.heigth_rocket = []  # [m]
-        self.angle_rocket = []  # [grad]
-        self.thrust = []  # [N]
-        self.drag = []  # [N]
-        self.gravity = []  # [N]
-        self.force_res = []  # [N]
-        self.force_res_split = []  # [(N, N)] for x and y direction
-        self.temperature = []  # [K]
-        self.pressure = []  # [Pa]
-        self.density = []  # [kg/m^3]
+        self.time = [0]  # [s]
+        self.pos_x_rocket = []  # [m]
+        self.pos_y_rocket = []  # [m]
+        self.velocity_rocket = [0.0]  # [m/s]
+        self.acceleration_rocket = [0.0]  # [m/s^2]
+        self.mass_rocket = [0.0]  # [kg]
+        self.heigth_rocket = [0.0]  # [m]
+        self.angle_rocket = [0.0]  # [grad]
+        self.thrust = [0.0]  # [N]
+        self.drag = [0.0]  # [N]
+        self.gravity = [0.0]  # [N]
+        self.force_res = [0.0]  # [N]
+        self.force_res_split = [0.0]  # [(N, N)] for x and y direction
+        self.temperature = [0.0]  # [K]
+        self.pressure = [0.0]  # [Pa]
+        self.density = [0.0]  # [kg/m^3]
 
-    def add_data(self, time, pos_rocket, velocity_rocket, acceleration_rocket, mass_rocket, height_rocket,
+    def add_data(self, time, pos_x_rocket, pos_y_rocket, velocity_rocket, acceleration_rocket, mass_rocket, height_rocket,
                  angle_rocket, thrust, drag, gravity, force_res, force_res_split, temperature, pressure, density):
         """
         Appends new data values to their list
         :param time: Time the simulation ran (Double)
-        :param pos_rocket: Position of the rocket as coordinates (Tuple -> (Double, Double))
+        :param pos_x_rocket: Position of the rocket as coordinates (Double)
+        :param pos_y_rocket: Position of the rocket as coordinates (Double)
         :param velocity_rocket: Velocity of the rocket (Double)
         :param acceleration_rocket:  Acceleration of the rocket (Double)
         :param mass_rocket: Mass of the rocket (Double)
@@ -48,7 +50,8 @@ class Data(object):
         :param density: Density of the medium the rocket is in (Double)
         """
         self.time.append(time)
-        self.pos_rocket.append(pos_rocket)
+        self.pos_x_rocket.append(pos_x_rocket)
+        self.pos_y_rocket.append(pos_y_rocket)
         self.velocity_rocket.append(velocity_rocket)
         self.acceleration_rocket.append(acceleration_rocket)
         self.mass_rocket.append(mass_rocket)
@@ -70,7 +73,8 @@ class Data(object):
         with open(self.data_file, 'wb') as csv_file:
             data_writer = csv.writer(csv_file, delimiter=',')
             data_writer.writerow(self.time)
-            data_writer.writerow(self.pos_rocket)
+            data_writer.writerow(self.pos_x_rocket)
+            data_writer.writerow(self.pos_y_rocket)
             data_writer.writerow(self.velocity_rocket)
             data_writer.writerow(self.acceleration_rocket)
             data_writer.writerow(self.mass_rocket)
@@ -95,17 +99,18 @@ class Data(object):
             for row in data_reader:
                 new_data.append(row)
             self.time = new_data[0]
-            self.pos_rocket = new_data[1]
-            self.velocity_rocket = new_data[2]
-            self.acceleration_rocket = new_data[3]
-            self.mass_rocket = new_data[4]
-            self.heigth_rocket = new_data[5]
-            self.angle_rocket = new_data[6]
-            self.thrust = new_data[7]
-            self.drag = new_data[8]
-            self.gravity = new_data[9]
-            self.force_res = new_data[10]
-            self.force_res_split = new_data[11]
-            self.temperature = new_data[12]
-            self.pressure = new_data[13]
-            self.density = new_data[14]
+            self.pos_x_rocket = new_data[1]
+            self.pos_y_rocket = new_data[2]
+            self.velocity_rocket = new_data[3]
+            self.acceleration_rocket = new_data[4]
+            self.mass_rocket = new_data[5]
+            self.heigth_rocket = new_data[6]
+            self.angle_rocket = new_data[7]
+            self.thrust = new_data[8]
+            self.drag = new_data[9]
+            self.gravity = new_data[10]
+            self.force_res = new_data[11]
+            self.force_res_split = new_data[12]
+            self.temperature = new_data[13]
+            self.pressure = new_data[14]
+            self.density = new_data[15]
