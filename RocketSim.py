6e7978c1b7fe0e73fbe49f3021872cb4fdbe8c6f
Gronner <felix.braeunling@t-online.de>
2015-11-18 11:03:41 +0100

    Fixed logic in the simulation and expanded setup of the simulation

--- RocketSim.py
+++ RocketSim.py
@@ -41,18 +41,17 @@ class Flight(object):
         Runs the simulation and starts the calculations
         """
         # Save time
-        if len(self.data.time) == 0:
-            time_now = 0
-        else:
-            time_now = self.data.time[-1] + self.time_delta
+        time_now = self.data.time[-1] + self.time_delta
         self.data.time.append(time_now)
         # Get the current height the rocket is at
-        height_now = Formula.vector_addition(self.rocket.get_pos())
+        height_now = Formula.vector_addition(self.rocket.get_pos())-self.planet.get_radius()
+        distance_now = Formula.vector_addition(self.rocket.get_pos())
         self.data.heigth_rocket.append(height_now)  # Save current height
         # Get the temperature at the height of the rocket
         temp_low_now = self.atmosphere.get_layer(height_now).get_temp_low()
         temp_gradient_now = self.atmosphere.get_layer(height_now).get_temp_gradient()
-        height_low_now = self.atmosphere.get_layer(height_now).get_height_low()
+        index_layer = self.atmosphere.get_layers().index(self.atmosphere.get_layer(height_now))
+        height_low_now = self.atmosphere.calc_height_below(index_layer)
         temperature_now = Formula.temperature(temp_low_now, temp_gradient_now, height_now, height_low_now)
         self.data.temperature.append(temperature_now)  # Save current temperature
         # Get the pressure at the height of the rocket
@@ -60,7 +59,7 @@ class Flight(object):
         pressure_now = Formula.pressure(pressure_low_now, temp_gradient_now, height_now, height_low_now, temperature_now)
         self.data.pressure.append(pressure_now)  # Save current pressure
         # Get the current force of gravity the rocket is experiencing
-        gravity_now = Formula.gravity(self.planet.get_mass(), self.rocket.get_mass(), height_now)
+        gravity_now = Formula.gravity(self.planet.get_mass(), self.rocket.get_mass(), distance_now)
         self.data.gravity.append(gravity_now)
         # Get the thrust the rocket is producing
         current_stage = self.rocket.get_current_stage()
@@ -68,17 +67,18 @@ class Flight(object):
         if type(current_stage) == RocketPart:
             thrust_now = current_stage.get_thrust()
         else:
-            thrust_now = Formula.thrust(current_stage.get_mass_change(), current_stage.get_velocity_exhaust,
-                                        current_stage.get_surface_nozzle, current_stage.get_pressure_nozzle,
+            thrust_now = Formula.thrust(current_stage.get_mass_change(), current_stage.get_velocity_exhaust(),
+                                        current_stage.get_surface_nozzle(), current_stage.get_pressure_nozzle(),
                                         pressure_now)
             change_prop_mass = True
         self.data.thrust.append(thrust_now)
         # Get the drag the rocket is experiencing
         density_now = Formula.density(pressure_now, temperature_now)
+        self.data.density.append(density_now)  # Save density
         velocity_before = Formula.vector_addition(self.rocket.get_velocity())
-        drag_coefficient = self.rocket.rocket_parts[0].drag_coefficient
+        drag_coefficient = self.rocket.rocket_parts[0].drag_coefficient_part
         drag_now = Formula.drag(density_now, velocity_before, self.rocket.get_surface(), drag_coefficient)
-        self.data.drag.append(drag_now) # Save current drag
+        self.data.drag.append(drag_now)  # Save current drag
         # Get current resulting force and resulting force in x and y direction
         force_res_now = Formula.resulting_force(thrust_now, drag_now, gravity_now)
         self.data.force_res.append(force_res_now)  # Save current resulting force
@@ -86,7 +86,7 @@ class Flight(object):
         self.data.angle_rocket.append(angle_rocket_now)  # Save current angle of the rocket
         force_res_x_now = Formula.res_x(force_res_now, angle_rocket_now)
         force_res_y_now = Formula.res_y(force_res_now, angle_rocket_now)
-        self.data.force_res_split([force_res_x_now, force_res_y_now]) # Save split resulting force
+        self.data.force_res_split.append([force_res_x_now, force_res_y_now]) # Save split resulting force
         # Get current acceleration
         acceleration_now = Formula.acceleration(force_res_now, self.rocket.get_mass())
         self.data.acceleration_rocket.append(acceleration_now) # Save current acceleration
@@ -101,7 +101,7 @@ class Flight(object):
             else:
                 current_stage.set_mass_propellant(mass_propellant_now)
             self.rocket.set_mass()
-            self.data.mass_rocket.append(self.rocket.get_mass())
+        self.data.mass_rocket.append(self.rocket.get_mass())
         # Get current velocity
         velocity_x_now = Formula.velocity(self.rocket.get_velocity()[0], self.time_delta, acceleration_x_now)
         velocity_y_now = Formula.velocity(self.rocket.get_velocity()[1], self.time_delta, acceleration_y_now)
@@ -113,10 +113,12 @@ class Flight(object):
         pos_x_now = Formula.position(self.rocket.get_pos()[0], way_traveled_x_now)
         way_traveled_y_now = Formula.way(self.time_delta, velocity_y_now, acceleration_y_now)
         pos_y_now = Formula.position(self.rocket.get_pos()[1], way_traveled_y_now)
-        self.data.pos_rocket.append([pos_x_now, pos_y_now]) # Save position of the rocket
+        self.data.pos_x_rocket.append(pos_x_now)  # Save x position of the rocket
+        self.data.pos_y_rocket.append(pos_y_now)  # Save y position of the rocket
         self.rocket.set_pos(pos_x_now, pos_y_now)
         # Get distance to planet core
         self.distance = Formula.vector_addition([pos_x_now, pos_y_now])
+        print height_now/1000.0
 
     def get_distance(self):
         """
@@ -132,29 +134,42 @@ def main():
     :return:
     """
     # Setup here
-    sim_planet = Planet(pos_planet=[0.0, 0.0], mass_planet=, radius_planet=)
-    sim_radius_planet = sim_planet.get_radius()
-    sim_layer_one = Layer(pressure_low=, width_layer=, temp_gradient=, temp_low=)
+    sim_planet = Planet(pos_planet=[0.0, 0.0], mass_planet=5.974e+24, radius_planet=12756.32*1000)
+    sim_planet_radius = sim_planet.get_radius()
+    sim_layer_one = Layer(pressure_low=100000, width_layer=18000.0, temp_gradient=-0.00065, temp_low=279.0)
     sim_atmosphere = Atmosphere()
     sim_atmosphere.add_layer(sim_layer_one)
     sim_data = Data(data_file="./Results/Test_one.csv")
-    sim_rocket_part_one = RocketPart(mass_part=, surface_part=, drag_coefficient_part=)
-    sim_tank_one = Tank(mass_part=, surface_part=, drag_coefficient_part=, mass_propellant=, mass_change_tank=,
-                        velocity_exhaust_tank=, surface_nozzle=, pressure_nozzle=)
-    sim_rocket = Rocket(pos=[0.0, sim_radius_planet], velocity=[0.0, 0.0], acceleration=[0.0, 0.0])
+    sim_rocket_part_one = RocketPart(mass_part=100000.0, surface_part=1.2, drag_coefficient_part=0.34)
+    sim_tank_one = Tank(mass_part=100.0, surface_part=0.0, drag_coefficient_part=0.0, mass_propellant=1000.0, mass_change_tank=0.34,
+                        velocity_exhaust_tank=30.0, surface_nozzle=0.8, pressure_nozzle=26000000.0)
+    sim_rocket = Rocket(pos=[sim_planet_radius, 0.0], velocity=[0.0, 0.0], acceleration=[0.0, 0.0])
     sim_rocket.append_part(sim_rocket_part_one)
     sim_rocket.append_part(sim_tank_one)
+    sim_rocket.set_mass()
+    sim_rocket.set_surface()
     sim_time_step = 0.1  # [s] Timestep the simulation uses
     sim_flight = Flight(sim_time_step, sim_planet, sim_rocket, sim_atmosphere, sim_data)
-    sim_max_step = 1000000000000000  # Maximum time steps the simulation should run
-    #Running the simulation
+    sim_max_step = 100000  # Maximum time steps the simulation should run
+    # Running the simulation
     current_step = 0
+    sim_flight.data.pos_x_rocket.append(sim_rocket.get_pos()[0])
+    sim_flight.data.pos_y_rocket.append(sim_rocket.get_pos()[1])
+    sim_flight.data.velocity_rocket.append(sim_rocket.get_velocity()[0])
+    sim_flight.data.acceleration_rocket.append(sim_rocket.get_acceleration()[0])
+    sim_flight.data.mass_rocket.append(sim_rocket.get_mass())
+    sim_flight.data.angle_rocket.append(sim_rocket.get_angle())
+    sim_flight.data.gravity.append(Formula.gravity(sim_planet.get_mass(), sim_rocket.get_mass(), sim_planet_radius))
+    sim_flight.data.force_res.append(-1*sim_flight.data.gravity[0])
+    sim_flight.data.temperature.append(sim_atmosphere.get_layer(0.0).get_temp_low())
+    sim_flight.data.pressure.append(sim_atmosphere.get_layer(0.0).get_pressure_low())
+    sim_flight.data.density.append(sim_atmosphere.get_layer(0).get_density_now(0.0, 0.0))
     print "Simulation starting"
-    while not (sim_flight.get_distance() < sim_radius_planet) or not (current_step > sim_max_step):
-        Flight.simulate()
+    while (not (sim_flight.get_distance() < sim_planet_radius)) and (not (current_step > sim_max_step)):
+        sim_flight.simulate()
         current_step += 1
     print "Simulation ended!"
-    Flight.data.write_csv()
-
+    sim_flight.data.write_csv()
+    print "Saved to CSV!"
 if __name__ == "__main__":
     main()
